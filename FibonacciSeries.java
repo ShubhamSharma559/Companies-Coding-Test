57008c5111e6f2f2012201df4aeada17b97a5a65
Shubham Sharma <61112356+ShubhamSharma559@users.noreply.github.com>
2021-09-05 23:47:58 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
+import java.lang.*;
 public class Main
 {
 	public static void main(String[] args) {
 	    
 	    Scanner sc = new Scanner(System.in);
 	    int n = sc.nextInt();
-	    int first = 1, second = 1;   // to  start the factorial from zero, set first = 0
+	    int first = 1, second = 1;
 	    int third = first + second;
-	    for(int i = n;i>3;i--){
-	        int temp = third;
-	        third = third+second;
-	        second = temp;
+	    if(n>3)
+	    {
+	        for(int i = n;i>3;i--){
+	            int temp = third;
+	            third = third+second;
+	            second = temp;
+	        }
 	    }
+	    if(n == 1)
+	        third = first;
+	    else if(n==2)
+	        third = second;
+	    else if(n==3)
+	        third = third;
+	    else{
+	        System.out.println("Invalid index");
+	        System.exit(0);
+	    }
+	        
 		System.out.println(third);
 	}
 }
